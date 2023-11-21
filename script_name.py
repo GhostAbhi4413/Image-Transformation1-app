c3091d14625dd4e04b931285d19b9f54ed54173e
Abhishek Nikam <96689091+GhostAbhi4413@users.noreply.github.com>
2023-11-21 15:07:22 +0530

    Update script_name.py

--- script_name.py
+++ script_name.py
@@ -1,38 +1,64 @@
-import streamlit as st
 import cv2
 import numpy as np
+import streamlit as st
 
-st.title("Image Transformations WebApp")
-st.markdown("", unsafe_allow_html=True)
+def enhance_image(image, contrast_factor, brightness_factor, smoothing_kernel_size, sharpening_factor, mask_image, morph_operation):
+    # Adjust contrast and brightness
+    enhanced_image = cv2.convertScaleAbs(image, alpha=contrast_factor, beta=brightness_factor)
 
-image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+    # Smoothening
+    if smoothing_kernel_size > 0:
+        enhanced_image = cv2.GaussianBlur(enhanced_image, (smoothing_kernel_size, smoothing_kernel_size), 0)
 
-if image is not None:
-    image = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
-    height, width = image.shape[:2]
+    # Sharpening
+    if sharpening_factor > 0:
+        kernel = np.array([[-1, -1, -1],
+                           [-1, 9 + sharpening_factor, -1],
+                           [-1, -1, -1]])
+        enhanced_image = cv2.filter2D(enhanced_image, -1, kernel)
 
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    # Masking
+    if mask_image is not None:
+        mask_image = cv2.cvtColor(mask_image, cv2.COLOR_BGR2GRAY)
+        _, mask = cv2.threshold(mask_image, 30, 255, cv2.THRESH_BINARY)
+        enhanced_image = cv2.bitwise_and(enhanced_image, enhanced_image, mask=mask)
 
-    translation_matrix = np.float32([[1, 0, 50], [0, 1, 30]])
+    # Morphological operations
+    if morph_operation == 'Dilation':
+        kernel = np.ones((5, 5), np.uint8)
+        enhanced_image = cv2.dilate(enhanced_image, kernel, iterations=1)
+    elif morph_operation == 'Erosion':
+        kernel = np.ones((5, 5), np.uint8)
+        enhanced_image = cv2.erode(enhanced_image, kernel, iterations=1)
 
-    angle = st.slider("Rotation Angle (degrees)", -180, 180, 0)
-    rotation_matrix = cv2.getRotationMatrix2D((width / 2, height / 2), angle, 1)
+    return enhanced_image
 
-    scale_x = st.slider("Scale X", 0.1, 3.0, 1.0)
-    scale_y = st.slider("Scale Y", 0.1, 3.0, 1.0)
-    scaling_matrix = np.float32([[scale_x, 0, 0], [0, scale_y, 0]])
+# Streamlit UI
+st.title("Image Enhancement App")
 
-    shear_x = st.slider("Shear X", -1.0, 1.0, 0.0)
-    shear_y = st.slider("Shear Y", -1.0, 1.0, 0.0)
-    shear_matrix = np.float32([[1, shear_x, 0], [shear_y, 1, 0]])
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-    if st.button("Apply Transformations"):
-        translated_image = cv2.warpAffine(image, translation_matrix, (width, height))
-        rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
-        scaled_image = cv2.warpAffine(image, scaling_matrix, (width, height))
-        sheared_image = cv2.warpAffine(image, shear_matrix, (width, height))
+if uploaded_file is not None:
+    image = cv2.imread(uploaded_file.name)
 
-        st.image(translated_image, caption="Translated Image", use_column_width=True)
-        st.image(rotated_image, caption="Rotated Image", use_column_width=True)
-        st.image(scaled_image, caption="Scaled Image", use_column_width=True)
-        st.image(sheared_image, caption="Sheared Image", use_column_width=True)
+    st.subheader("Original Image")
+    st.image(image, channels="BGR")
+
+    # User input for enhancement parameters
+    contrast_factor = st.slider("Contrast", 0.5, 2.0, 1.0)
+    brightness_factor = st.slider("Brightness", -50, 50, 0)
+    smoothing_kernel_size = st.slider("Smoothing Kernel Size", 0, 15, 0)
+    sharpening_factor = st.slider("Sharpening", 0, 5, 0)
+
+    mask_image = st.file_uploader("Upload a mask image (optional)...", type=["jpg", "jpeg", "png"])
+
+    morph_operations = ['None', 'Dilation', 'Erosion']
+    morph_operation = st.selectbox("Morphological Operation", morph_operations)
+
+    if st.button("Enhance Image"):
+        if mask_image is not None:
+            mask_image = cv2.imread(mask_image.name)
+        enhanced_image = enhance_image(image, contrast_factor, brightness_factor, smoothing_kernel_size, sharpening_factor, mask_image, morph_operation)
+
+        st.subheader("Enhanced Image")
+        st.image(enhanced_image, channels="BGR", use_column_width=True)
